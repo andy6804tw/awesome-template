4d09497f744df0183339d0e68cc4fa21af2f5218
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-03-14 14:42:16 +0800

    add card slider material

---
+++ card-slider/script.js
@@ -0,0 +1,69 @@
+$('.slide-nav').on('click', function(e) {
+  e.preventDefault();
+  // get current slide
+  var current = $('.flex--active').data('slide'),
+    // get button data-slide
+    next = $(this).data('slide');
+
+  $('.slide-nav').removeClass('active');
+  $(this).addClass('active');
+
+  if (current === next) {
+    return false;
+  } else {
+    $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+    $('.flex--active').addClass('animate--end');
+    setTimeout(function() {
+      $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+      $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+    }, 800);
+  }
+});
+
+
+// document.body.onscroll = function (event) {
+//   const html = document.documentElement;
+//   console.log(html.scrollTop);
+// };
+
+let index=1;
+let animating=false;
+window.addEventListener('wheel', function (event) {
+  console.log(animating);
+  if (animating) return;
+  animating = true;
+  
+  if (event.deltaY > 0) {
+    console.log('scrolling up');
+    index-=1;
+    // document.getElementById('status').textContent = 'scrolling up';
+  } else if (event.deltaY < 0) {
+    console.log('scrolling down');
+    index+=1;
+    // document.getElementById('status').textContent = 'scrolling down';
+  }
+  if (index < 1)
+    index = 5;
+  else if (index > 5)
+    index = 1;
+  $('.slider__warpper').find('.flex__container[data-slide=' + index + ']').addClass('flex--preStart');
+  $('.flex--active').addClass('animate--end');
+  setTimeout(function () {
+    $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+    $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+  }, 800);
+  setTimeout(function(){
+    animating = false;
+  },1800)
+});
+
+
+
+// window.addEventListener('wheel', function (event) {
+//   // console.log(event.deltaY)
+//   if (event.deltaY < 0) {
+//     console.log('scrolling up');
+//   } else if (event.deltaY > 300) {
+//     console.log('scrolling down');
+//   }
+// });
