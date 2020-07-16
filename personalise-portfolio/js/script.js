5c91cd32415781812c8e581bcddfaa810fd70f7c
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2020-07-16 23:02:32 +0800

    modify asymmetrical-layout css

---
+++ personalise-portfolio/js/script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  document.querySelector('.loader').style.opacity = 0;
+  const menu = document.querySelector('.menu');
+
+  menu.addEventListener('click', () =>
+    document.querySelector('ul').classList.toggle('open')
+  );
+};
