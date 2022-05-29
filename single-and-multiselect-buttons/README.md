1abca9206076d06d15b1600d88cf2f9aacdc1471
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2022-05-29 17:00:43 +0800

    add single-and-multiselect-buttons

---
+++ single-and-multiselect-buttons/README.md
@@ -0,0 +1,4 @@
+# Single and multiselect buttons
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/kiprutobrian/pen/YzyOKjB](https://codepen.io/kiprutobrian/pen/YzyOKjB).
+
---
+++ single-and-multiselect-buttons/dist/index.js
@@ -0,0 +1,30 @@
+function getResult() {
+  // 取得多選區塊內容
+  let question1List = [];
+  const checkboxes = document.querySelectorAll(
+    'input[name="question1Checkbox"]:checked'
+  );
+  for (var i = 0; i < checkboxes.length; i++) {
+    question1List.push(checkboxes[i].value);
+  }
+  // 取得單選區塊內容
+  const question2Value = document.querySelector(
+    'input[name="question2Checkbox"]:checked'
+  ).value;
+
+  alert(`Multiple Select: ${question1List}\n Single Option: ${question2Value}`);
+}
+
+const inputButtons = document.querySelectorAll('input');
+Array.from(inputButtons).forEach((button, index) => {
+  button.addEventListener('click', () => {
+    // 取得多選區塊被選擇的DOM
+    const checkboxes = document.querySelectorAll('input[name="question1Checkbox"]:checked');
+    // 取得單選區塊被選擇的DOM
+    const radioboxes = document.querySelectorAll('input[name="question2Checkbox"]:checked');
+    // 透過計數檢查兩嵙區塊是否都有選擇按鈕
+    if (checkboxes.length & radioboxes.length) {
+      document.getElementById('btnSubmit').classList.remove('disabled');
+    }
+  });
+});
